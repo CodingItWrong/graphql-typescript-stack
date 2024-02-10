b8a80cd3d9fbe77e2f048298e084359efa9aea56
Josh Justice <me@codingitwrong.com>
2024-02-10 11:44:07 -0500

    Add client

---
+++ web/src/App.tsx
@@ -0,0 +1,22 @@
+import {useGetUsersQuery} from './generated/graphql';
+
+function App() {
+  const {loading, error, data} = useGetUsersQuery();
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  if (!data) return <p>Data is undefined??</p>;
+
+  return (
+    <div className="App">
+      <h1>Users</h1>
+      <ul>
+        {data.users.map(user => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
