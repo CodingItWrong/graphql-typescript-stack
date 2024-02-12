b2b90245cda2bca405434b912f2a9e8ba13b1a41
Josh Justice <me@codingitwrong.com>
2024-02-12 08:17:14 -0500

    Comment on cache behavior

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -2,6 +2,12 @@ import { Suspense } from "react";
 import { UserList } from "./UserList";
 
 export default function Home() {
+  /*
+   * NOTE: when you first start the Next server and load this page, you should see "Loading…"
+   * in the returned HTML, then see users outputted on the page without a GraphQL request to
+   * the server. If you then reload the page you'll see the users rendered from the start,
+   * because they are available immediately in the cache.
+   */
   return (
     <div className="App">
       <h1>Users</h1>
