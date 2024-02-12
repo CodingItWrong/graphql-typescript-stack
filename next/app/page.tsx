7082e5c17c653235b112f7d9aa325b6c195121da
Josh Justice <me@codingitwrong.com>
2024-02-12 08:02:23 -0500

    Run GraphQL query on server

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,26 +1,13 @@
-import { useSuspenseQuery } from "@apollo/client";
-import { GetUsersDocument } from "./generated/graphql";
 import { Suspense } from "react";
+import { UserList } from "./UserList";
 
 export default function Home() {
   return (
     <div className="App">
       <h1>Users</h1>
       <Suspense fallback={<p>Loading…</p>}>
-        <Users />
+        <UserList />
       </Suspense>
     </div>
-  );
-}
-
-function Users() {
-  const { data } = useSuspenseQuery(GetUsersDocument);
-
-  return (
-      <ul>
-        {data.users.map(user => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
   );
 }
