28043baf1185ffb1eb770d8c0dfdaceec667fcee
Josh Justice <me@codingitwrong.com>
2024-02-15 08:28:16 -0500

    Migrate DB to be not null

--- api/src/db/schema.ts
+++ api/src/db/schema.ts
@@ -2,8 +2,8 @@ import { serial, text, timestamp, pgTable } from "drizzle-orm/pg-core";
 
 export const user = pgTable("user", {
   id: serial("id"),
-  name: text("name"),
-  email: text("email"),
+  name: text("name").notNull(),
+  email: text("email").notNull(),
   password: text("password"),
   role: text("role").$type<"admin" | "customer">(),
   createdAt: timestamp("created_at"),
